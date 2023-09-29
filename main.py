82a06efcb21a6dd360d62d99ede4896ca9aa67ab
Rafal Michal Czerwik <rafal.czerwik@gmail.com>
2023-09-29 13:26:40 +0200

    update 2
    Writed code.

---
+++ main.py
@@ -0,0 +1,30 @@
+import random
+
+
+def get_user_guess():
+    """Get number from user and return it."""
+    while True:
+        try:
+            user_guess = input('Guess the number: ')
+            if user_guess.isdigit():
+                return int(user_guess)
+        except ValueError:
+            print("It's not a number!")
+
+
+def main():
+    """Main game function."""
+    guess_number = random.randint(1, 100)
+    while True:
+        guess = get_user_guess()
+        if guess < guess_number:
+            print("Too small!")
+        elif guess > guess_number:
+            print("Too big!")
+        else:
+            print("You win!!!")
+            break
+
+
+if __name__ == "__main__":
+    main()
